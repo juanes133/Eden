160cbca005c8cb862ca0102e463b97b9bb4b4c28
Yuber Garcia <yubergarcia@veryfi.com>
2022-08-25 07:26:59 -0500

    Se adelanto parte de la funcionalidad de notificacion de conexion a internet  en la app.

--- app/src/main/java/com/jadevelopers/eden/MainActivity.kt
+++ app/src/main/java/com/jadevelopers/eden/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.jadevelopers.eden
 
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.fragment.findNavController
 import com.jadevelopers.eden.databinding.ActivityMainBinding
+import com.jadevelopers.eden.databinding.FragmentLoginBinding
+import com.jadevelopers.eden.view.HomeFragment
+import com.jadevelopers.eden.view.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var checkNetworkConnection: CheckNetworkConnection
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        callNetworkConnection()
+    }
+    private fun callNetworkConnection() {
+        checkNetworkConnection = CheckNetworkConnection(application)
+        checkNetworkConnection.observe(this) { isConnected ->
+            if (isConnected) {
+
+            } else {
+                AlertDialog.Builder(this).setTitle("No Internet Connection")
+                    .setMessage("Please check your internet connection and try again")
+                    .setPositiveButton(android.R.string.ok) { _, _ -> }
+                    .setIcon(android.R.drawable.ic_dialog_alert).show()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/jadevelopers/eden/view/LoginFragment.kt
+++ app/src/main/java/com/jadevelopers/eden/view/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.jadevelopers.eden.view
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
