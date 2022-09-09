d631199fae0f09bc335dacd3f680ef631289d28b
Yuber Garcia <yubergarcia@veryfi.com>
2022-09-09 18:24:09 -0500

    se trabajo en el drawer, pero no se logra el objetivo en la ejecución

--- app/src/main/java/com/jadevelopers/eden/MainActivity.kt
+++ app/src/main/java/com/jadevelopers/eden/MainActivity.kt
@@ -3,7 +3,13 @@ package com.jadevelopers.eden
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.jadevelopers.eden.databinding.ActivityMainBinding
@@ -11,7 +17,11 @@ import com.jadevelopers.eden.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private var dialog: AlertDialog? = null
     private lateinit var binding: ActivityMainBinding
-    private lateinit var checkNetworkConnection: CheckNetworkConnection
+    private var checkNetworkConnection: CheckNetworkConnection? = null
+    private var drawerLayout: DrawerLayout? = null
+    private var appBarConfiguration: AppBarConfiguration? = null
+    private var navController: NavController? = null
+    private var navigationView: NavigationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +33,26 @@ class MainActivity : AppCompatActivity() {
             .setMessage("Comprueba tu conexión a Internet e inténtalo de nuevo")
             .setIcon(android.R.drawable.ic_dialog_alert)
             .create()
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        drawerLayout = binding.drawerLayout
+        navController?.let {
+            navigationView?.setupWithNavController(it)
+        }
+        appBarConfiguration = navController?.let {
+            AppBarConfiguration(it.graph, drawerLayout)
+        }
+        appBarConfiguration?.let { appBarConfiguration ->
+            navController?.let {
+                setupActionBarWithNavController(it, appBarConfiguration)
+            }
+        }
     }
 
     private fun callNetworkConnection() {
         checkNetworkConnection = CheckNetworkConnection(application)
-        checkNetworkConnection.observe(this) { isConnected ->
+        checkNetworkConnection?.observe(this) { isConnected ->
             if (isConnected) {
                 dialog?.dismiss()
             } else {
@@ -42,9 +67,9 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
         if (navController.currentDestination?.id == R.id.homeFragment) {
-            if (Firebase.auth.currentUser != null){
+            if (Firebase.auth.currentUser != null) {
                 finish()
-            }else{
+            } else {
                 super.onBackPressed()
             }
         } else {
