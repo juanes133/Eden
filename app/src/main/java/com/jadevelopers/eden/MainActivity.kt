e6129473d82c348cc89b9ce8b64b50ffb1121032
Yuber Garcia <yubergarcia@veryfi.com>
2022-09-19 17:11:50 -0500

    Se realizaron las mejoras propuestas en el spring 4 y se esta creando una nueva vista para productos

--- app/src/main/java/com/jadevelopers/eden/MainActivity.kt
+++ app/src/main/java/com/jadevelopers/eden/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.jadevelopers.eden
 
 import android.os.Bundle
-import android.view.Gravity
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -26,8 +24,6 @@ class MainActivity : AppCompatActivity() {
     private var appBarConfiguration: AppBarConfiguration? = null
     private var navController: NavController? = null
     private var navigationView: NavigationView? = null
-    lateinit var toogle: ActionBarDrawerToggle
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -90,9 +86,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
-        if (Firebase.auth.currentUser == null) {
+        if (drawerLayout?.isDrawerOpen(GravityCompat.START) == true) {
+            drawerLayout?.closeDrawer(GravityCompat.START)
+        } else if (Firebase.auth.currentUser == null) {
             finish()
+        }else{
+            super.onBackPressed()
         }
     }
 }
