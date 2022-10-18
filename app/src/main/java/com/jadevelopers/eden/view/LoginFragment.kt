b0075e3e025cfcf50628882bd87ab15bb1ed36b3
Juan <juanes1331@gmail.com>
2022-10-17 20:08:45 -0500

    se centró imagen de drawermenu

--- app/src/main/java/com/jadevelopers/eden/view/LoginFragment.kt
+++ app/src/main/java/com/jadevelopers/eden/view/LoginFragment.kt
@@ -6,8 +6,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewTreeObserver
-import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
